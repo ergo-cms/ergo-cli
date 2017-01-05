53be63281a54cc6629106bfbedd691278873d08b
cmroanirgo <cmroanirgo@users.noreply.github.com>
2017-01-05 11:52:41 +1100

    added --clean

--- api/build.js
+++ api/build.js
@@ -12,10 +12,13 @@
 module.exports = require('./api-base')(
  		'build', 				// this api method name
  		undefined, 				// no required parameters
-		undefined,			// commandline aliases
+		{'c':'clean'},			// commandline aliases
 		"\tbuild                  Builds the source and places them in the output dir\n"+
 		"\t                       eg. 'ergo build'\n"+
-		"" 		
+		"\t-c, --clean            Removes all files from the output folder beforehand.\n"+
+		"\t                       Use with EXTREME caution if on a live server\n" +
+//		"\t                       Use use an .ergoignore file to keep some files.\n" + 
+		""
  	);
 
 module.exports.build = function(options) {
